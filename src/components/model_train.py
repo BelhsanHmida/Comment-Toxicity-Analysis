a8407b141eab6b7f0ec8b3b40d9287cb036043f2
BelhsanHmida <mohamedbelhsanhmida@gmail.com>
2024-04-09 15:36:21 +0100

    almost there

--- src/components/model_train.py
+++ src/components/model_train.py
@@ -1,9 +1,60 @@
+import sys
+sys.path.append(r'C:\Users\hp\Desktop\CommentToxicity\Comment-Toxicity-Classification')
+import os
 import tensorflow as tf
-import keras
+from dataclasses import dataclass
 
+from keras.models import Sequential
+from keras.layers import LSTM, Dropout, Bidirectional, Dense, Embedding
+from keras.layers import Dropout, BatchNormalization
 
-# Load the model
-model = tf.keras.models.load_model(r'C:\Users\hp\Desktop\Datascience  Projects\CommentToxicity\artifact\my_model1.keras')
+from src.logger import logging
+from src.exceptions import CustomException
+from src.utils import save_objects
+from src.components.data_ingestion import DataIngestion
+@dataclass
+class modeltrainerconfig:
+    trained_model_path=os.path.join('artifact','model.h5')
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = modeltrainerconfig()
+    def initiate_model_trainer(self,train, val, test):    
 
-# Optionally, you can also print a summary of the model
-model.summary()
+        MAX_FEATURES = 2000000 
+        model = Sequential()
+        # Create the embedding layer
+        model.add(Embedding(MAX_FEATURES + 1, 32))
+
+        # Add dropout after the embedding layer
+        model.add(Dropout(0.2))  # Adjust dropout rate as needed
+
+        model.add(Bidirectional(LSTM(32, activation='tanh')))
+
+        # Add dropout after the LSTM layer
+        model.add(Dropout(0.2))  # Adjust dropout rate as needed
+
+        model.add(Dense(128, activation='relu'))
+        model.add(BatchNormalization())  # Add batch normalization
+
+        model.add(Dense(256, activation='relu'))
+        model.add(Dense(128, activation='relu'))
+
+        # Add dropout before the final output layer
+        model.add(Dropout(0.5))  # Adjust dropout rate as needed
+
+        # Final Layer maps to output 
+        model.add(Dense(6, activation='sigmoid'))
+        self.model=model
+        def ModelCompile():
+            self.model.compile(optimizer='adam', loss='binary_crossentropy')
+            
+        def ModelFit():
+            self.model.fit(train, epochs=1, validation_data=val)
+            
+        model.save(self.model_trainer_config.trained_model_path, save_format="h5")
+        logging.info(f"Model saved at {self.model_trainer_config.trained_model_path}")
+
+if __name__ =='__main__':
+    train, test, val = DataIngestion().initiate_data_ingestion()
+    model_trainer = ModelTrainer()
+    model_trainer.initiate_model_trainer(train, val, test)
---
+++ src/pipelines/predict_pipeline.py
@@ -0,0 +1,24 @@
+#After we trained our model from the model_train we can now mae predictions using the predict_pipeline.py
+import sys
+sys.path.append(r'C:\Users\hp\Desktop\CommentToxicity\Comment-Toxicity-Classification')
+import os
+import pickle
+import tensorflow as tf
+import pandas as pd  # Example library, import any other necessary libraries for your prediction pipeline
+import numpy as np
+class CommentToxicityPredictor:
+  
+    def predict_toxicity(self,df_comments):
+        #model_path = "artifact/model.h5"  # Update with the path to your saved model file
+        #self.model = tf.keras.models.load_model(model_path)
+        
+        #vectorizer_file = "Comment-Toxicity-Classification\artifact\vectorizer.pkl"
+        #with open(vectorizer_file, "rb") as f:
+         #   vectorizer = pickle.load(f)
+ 
+        #self.X = vectorizer(df_comments['comment_text'].values)
+        #y_pred = self.model.predict(self.X)
+        y_pred = pd.DataFrame(np.random.rand(100, 6), columns=['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate'])
+
+        return y_pred
+    
